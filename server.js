6c92f87ce71ee5519c99d7c53f71af87b1b60bb3
pppreap <pppreap@icloud.net>
2022-08-30 13:29:58 -0400

    edited server files to include handlebars reference

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 // import sequelize connection
 const sequelize = require('./config/connection');
+
+const helpers = require('./utils/helpers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +17,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
