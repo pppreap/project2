c732f52bdaedb6277c2f44bc5309ec13a172993a
Christian Kay <Christiankay@rockconnections.com>
2022-09-06 21:37:00 -0400

    Font coloring change

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,9 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  // : new Sequelize(process.env.DB_NAME, 
-  //                 process.env.DB_USER, 
-  //                 process.env.DB_PW, 
-  : new Sequelize ( "theweighin_db",
-  "root", "pwpw",
+   : new Sequelize(process.env.DB_NAME, 
+                  process.env.DB_USER, 
+                 process.env.DB_PW, 
     {
       host: 'localhost',
       dialect: 'mysql',
